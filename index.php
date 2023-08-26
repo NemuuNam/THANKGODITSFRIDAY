9804cf1d02f017cb35db012de8c15e84c40d2295
NemuuNam <64513830+NemuuNam@users.noreply.github.com>
2023-08-27 02:15:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,132 @@
+<?php
+// Get the current count from the JSON file
+$jsonData = file_get_contents('member_count.json');
+$countData = json_decode($jsonData, true);
+
+// Increment the member count
+$countData['member_count']++;
+
+// Encode the updated data and save it back to the JSON file
+$newJsonData = json_encode($countData, JSON_PRETTY_PRINT);
+file_put_contents('member_count.json', $newJsonData);
+
+// Get the member count from the JSON file
+$jsonData = file_get_contents('member_count.json');
+$countData = json_decode($jsonData, true);
+
+// Access the member count and assign it to a variable for use in your website
+$memberCount = $countData['member_count'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="Main.css">
+</head>
+
+<body>
+    <div class="grid-con-0">
+        <div class="grid-item-0">
+            <div class="grid-item-info-1">
+                <div>
+                    <h4>THANKGODIT'FRIDAY</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="grid-con-1">
+        <div class="grid-item-1">
+            <img
+                src="https://images.unsplash.com/photo-1601314002592-b8734bca6604?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjB8fGZvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60">
+        </div>
+        <div class="grid-item-2">
+            <div class="grid-item-info">
+                <div>
+                    <h2>Home</h2>
+                    <p>หน้าเว็บเริ่มต้น กลับไปที่หน้า Homeได้ที่นี่</p>
+                    <br>
+                    <center><button class="btn" onclick="window.location.href = '#';">
+                            Home
+                        </button></center>
+                </div>
+            </div>
+            <div>
+                <img
+                    src="https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGZvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60">
+            </div>
+        </div>
+        <div class="grid-item-3">
+            <div>
+                <img
+                    src="https://images.unsplash.com/photo-1478145046317-39f10e56b5e9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGZvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60">
+            </div>
+            <div class="grid-item-info">
+                <div>
+                    <h2>Recipe</h2>
+                    <p>สูตรอาหารมากมายรอคุณอยู่ หาสูตรอาหารเพิ่มเติมได้ที่นี่</p>
+                    <br>
+                    <center><button class="btn" onclick="window.location.href = 'file:///D:/webvscode/web/list.html';">
+                            Recipe
+                        </button></center>
+                </div>
+            </div>
+        </div>
+        <div class="grid-item-4">
+            <div>
+                <img
+                    src="https://images.unsplash.com/photo-1543362906-acfc16c67564?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTB8fGZvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60">
+            </div>
+            <div class="grid-item-info">
+                <div>
+                    <h2>Google</h2>
+                    <p>คุณสามารถหาข้อมูลเพิ่มเติมได้จาก Google</p>
+                    <br>
+                    <center><button class="btn" onclick="window.location.href = 'https://www.google.com';">
+                            Google
+                        </button></center>
+                </div>
+            </div>
+        </div>
+        <div class="grid-item-5">
+            <img
+                src="https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGZvb2R8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60">
+        </div>
+    </div>
+    <div class="grid-con-2">
+        <div class="grid-item-6">
+            <div class="grid-item-info-2">
+                <div>
+                    <footer>
+                        <div class="footer">
+                            <div class="row">
+                                <ul>
+                                    <li><a href="file:///D:/webvscode/web/index.html#">Home</a></li>
+                                    <li><a href="file:///D:/webvscode/web/Contact%20us.html">Contact us</a></li>
+                                    <li><a href="https://www.rmutp.ac.th/">มทร.พระนคร</a></li>
+                                    <li><a
+                                            href="https://bus.rmutp.ac.th/?fbclid=IwAR0W-n6IHIXHWFE-mn95U9AedJP8_R5kIG9Yj08KEarZhxOXnfGcqoJQkfs">คณะบริหารธุรกิจ</a>
+                                    </li>
+                                    <li><a href="file:///D:/webvscode/web/about.html">About</a></li>
+                                    <li><a href="https://calendar.online/88dd9937c4900fbe03b3">Calendar</a></li>
+                                </ul>
+                            </div>
+                            <div class="row">
+                                <div>THANKGODIT'SFRIDAY</div>
+                                <div class="num-count"><BR>
+                                    <p>Number of Members: <?php echo $memberCount; ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
